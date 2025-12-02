249f03dbfaafc9cddf20d676cef853514ea3da82
samanteras <samanter@mail.ru>
2025-12-02 11:29:36 +0300

    git commit -m "fix: increase timeouts for nginx and python to 5 min"

--- app.py
+++ app.py
@@ -206,4 +206,4 @@ async def supabase_webhook(
     )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=300)
